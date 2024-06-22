ac7b8d572149490ace2b957bc14f98a6dabb0c6f
lvicenteperea <lavp99@hotmail.com>
2024-06-22 17:04:58 +0200

    Subida para poder desplegar en produccion

--- Backend/FastAPI/db/client.py
+++ Backend/FastAPI/db/client.py
@@ -1,4 +1,8 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local ## sin nada se conecta a mongo en local
+#Base de datos LOCAL
+# db_client = MongoClient().local ## sin nada se conecta a mongo en local
 
+# Bases de dato remota al mongoAtlas (mongodb+srv://test:test@atlascluster.iovqvw0.mongodb.net/)
+db_client = MongoClient("mongodb+srv://test:test@atlascluster.iovqvw0.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster"
+                       ).test
