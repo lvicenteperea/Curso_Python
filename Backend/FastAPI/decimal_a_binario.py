5ecfd84e4ee853652597bd4b69637d89bdd3d6ac
lvicenteperea <lavp99@hotmail.com>
2024-06-23 19:14:45 +0200

    añadimos decimal_a_binario

---
+++ Backend/FastAPI/decimal_a_binario.py
@@ -0,0 +1,16 @@
+
+def decimal_a_binnario(decimal: int):
+
+    binario = ""
+
+    while decimal > 0:
+        binario = f"{decimal % 2}{binario}"
+        decimal = decimal / 2
+
+    return return "0" if binario == "" else binario
+
+
+decimal = int(input("Por favor, introduce un número entero: "))
+print(decimal_a_binnario(decimal))
+
+
